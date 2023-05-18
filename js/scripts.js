c5fc971c41bb040f2c891e852ba7c95069cf3a97
Brian Scherner <brianscherner@gmail.com>
2023-05-18 14:42:19 -0700

    Change variable for userSelectionArray to const from let. Declare variable for div id from HTML file and select form id 'grocery-list'. Set attribute for groceryForm variable. Make new array for sortedUserSelectionArray. Write forEach loop getting value from user selections and make them upper case. Edit other forEach loop method to match sortedUserSelectionsArray. Remove 'value' from list append method.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
 function handleForm(event) {
 	event.preventDefault();
-	const userSelections = document.querySelectorAll("input[name=grocery-item]:checked");
-	const userSelectionsArray = Array.from(userSelections);
-	const ul = document.querySelector("ul");
-	const alphaArray = userSelectionsArray.sort();
 
-	userSelectionsArray.forEach(function(element) {
+	const userSelections = document.querySelectorAll("input[name=grocery-item]:checked");
+	let userSelectionsArray = Array.from(userSelections);
+	const ul = document.querySelector("ul");
+	const groceryForm = document.getElementById("grocery-list");
+	groceryForm.setAttribute("class", "hidden");
+	let sortedUserSelectionsArray = [];
+	userSelectionsArray.forEach(element =>{
+		sortedUserSelectionsArray.push(element.value.toUpperCase());
+	});
+
+	sortedUserSelectionsArray.sort();
+
+	sortedUserSelectionsArray.forEach(function(element) {
 		const li = document.createElement("li");
-		li.append(element.value);
+		li.append(element);
 		ul.append(li);
 	});
 }
