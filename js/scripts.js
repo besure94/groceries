638edfc9857f14a4a1d02c083300d64438aa434b
Brian Scherner <brianscherner@gmail.com>
2023-05-18 11:23:04 -0700

    Make new array and alphabetize inputted items using sort method. Define variable for 'ul' selecting ul element. Add forEach loop method to create list items from array input values and append li items to ul.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,20 @@ function handleForm(event) {
 	event.preventDefault();
 	const userSelections = document.querySelectorAll("input[name=grocery-item]:checked");
 	const userSelectionsArray = Array.from(userSelections);
+	// const alphaArray = userSelectionsArray.sort();
 
 	userSelectionsArray.forEach(function(element) {
 		const paragraph = document.createElement("p");
 		paragraph.append(element.value);
 		document.body.append(paragraph);
+	});
+
+	const alphaArray = userSelectionsArray.sort();
+	const ul = document.querySelector("ul");
+	alphaArray.forEach(function(element) {
+		const li = document.createElement("li");
+		li.append(element);
+		ul.append(li);
 	});
 }
 
